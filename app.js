2908419b32d55c2015c0083ce845996f3a4de327
nazero3 <nazerrabah1994@gmail.com>
2022-09-29 08:44:49 +0300

    added couple pkges

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const http = require('http')
 //to create web server=> we need http module that has create server method 
-const server = http.createServer((req,res)=>{//careful for the order of the parameter it is important
-    res.write('whats up bruv')
-    res.end()
+
+const server = http.createServer((req,res)=>{
+    //careful for the order of the parameter it is important
+    if(req.url ==='/'){
+        res.end('whats up bruv')
+    }
+    else if(req.url === '/about'){
+        res.end('here is out story!')
+    }
+    else{
+        res.end('oops wrong page man')
+    }
+    
 })
 
 server.listen(5000)
+/*notes:
+- i tried to run the program with res.write inside req.url and the page was not loading
+- 
+
+*/
